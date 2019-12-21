775368a025c4b47d5df0c8fbccbf31a2bd955a34
nhuythuc <nhthuc1502@gmail.com>
2019-12-21 14:35:29 +0700

    no message

--- src/main/java/com/thucnh/azuredevops/AzuredevopsApplication.java
+++ src/main/java/com/thucnh/azuredevops/AzuredevopsApplication.java
@@ -2,9 +2,8 @@ package com.thucnh.azuredevops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"com.thucnh.azuredevops"})
+@SpringBootApplication
 public class AzuredevopsApplication {
 
 	public static void main(String[] args) {
