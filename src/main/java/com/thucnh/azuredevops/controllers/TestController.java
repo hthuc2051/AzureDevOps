e262e6b89476e5e5f408dee528c2f87d0e6b0bd6
nhuythuc <nhthuc1502@gmail.com>
2019-12-27 10:33:40 +0700

    no message

--- src/main/java/com/thucnh/azuredevops/controllers/TestController.java
+++ src/main/java/com/thucnh/azuredevops/controllers/TestController.java
@@ -17,6 +17,6 @@ public class TestController {
                 .body(generateString());
     }
     public String generateString(){
-        return "Updated String 23/12/2019 - Test Azure DevOps 11:06";
+        return "Updated String 27/12/2019 - Test Azure DevOps 11:06";
     }
 }
